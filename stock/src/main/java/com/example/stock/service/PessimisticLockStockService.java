a641879d199decdff2ad92ffb40993027dc22044
limsubinn <mymyx02@naver.com>
2024-06-13 15:40:12 +0900

    Feat: Pessimistic Lock 활용
    - MySQL을 이용한 동시성 문제 해결 방법
    - 실제로 데이터에 Lock을 걸어 정합성을 맞추는 방법

---
+++ stock/src/main/java/com/example/stock/service/PessimisticLockStockService.java
@@ -0,0 +1,27 @@
+package com.example.stock.service;
+
+import com.example.stock.domain.Stock;
+import com.example.stock.repository.StockRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class PessimisticLockStockService {
+
+    private final StockRepository stockRepository;
+
+    @Transactional
+    public void decrease(Long id, Long quantity) {
+        // Stock 조회
+        Stock stock = stockRepository.findByIdWithPessimisticLock(id)
+                .orElseThrow(() -> new RuntimeException("Stock을 조회할 수 없습니다."));
+
+        // 재고 감소
+        stock.decrease(quantity);
+
+        // 갱신된 값 저장
+        stockRepository.saveAndFlush(stock);
+    }
+}
